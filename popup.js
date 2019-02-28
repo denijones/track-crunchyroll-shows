547c2235121e8fc5180be0346f99352ef11dd7f4
Denisha Jones <DeniRJones@gmail.com>
2019-02-28 05:14:31 -0800

    Refactor with show objects

--- popup.js
+++ popup.js
@@ -1,42 +1,74 @@
 //Place episode history in a list
 let historyList = document.getElementById('historyList');
-//Put episodes in an array
-let episodesVisited = [];
-//Put anime shows into an array
-let showsVisited = [];
+//Array of objects to track shows and episodes visited
+let showList = [];
+//Store tab show and title
+let tabTitle, tabShow = "";
+
+
+/* Seperate show title and episode number into array
+Example: <title>JoJo's Bizarre Adventure: Golden Wind Episode 1 - Watch on Crunchyroll</title>
+0:"JoJo's Bizarre Adventure: Golden Wind", 1:"1" */
+function splitTitle(title){
+  return title.title.replace(" - Watch on Crunchyroll",'').split(' Episode');
+}
 
 //Use history API to search for crunchyroll objects & limit search results
 chrome.history.search(
   {text: "crunchyroll", maxResults: 5},
   //View the object results
-  function(episodes){
+  function(results){
     //Access each object
-      for(let i in episodes){
-        //Create a regex to find started shows (episode 1)
-        let regexShow = /\/episode-1/;
-        //Create a regex for episode numbers
-        let showName = episodes[i].title.replace(/ Episode \d+ - Watch on Crunchyroll/, '');
-        //Test URL's for matching "episode 1" regex
-        if(regexShow.test(episodes[i].url)){
-          //Add show to an array without duplicates
-          if(!(showName in showsVisited)){
-            showsVisited.push(showName);
+      for(let i in results){
+        let episodeNum = 0;
+        let showName = "";
+        let showPresent = false;
+        //Find URL's only from crunchyroll and only the show episodes, not episode list page
+        if(results[i].url.includes('crunchyroll.com') && results[i].title.includes('Episode')){
+          //Spilt the title for storing in object
+          let splitEpisode = splitTitle(results[i]);
+          //Change episode string number into int
+          episodeNum = parseInt(splitEpisode[1],10);
+          showName = splitEpisode[0];
+          //Add to array as object
+          for(let j in showList){
+            if(showName === showList[j].show){
+              showList[j].episodes.push(episodeNum);
+              //Find max episode, Math.max() cannot take the object's array so apply() will allow it
+              showList[j].last = Math.max.apply(Math,showList[j].episodes);
+              showPresent = true;
+            }
+          }
+          //If show isn't listed then add it in
+          if(showPresent === false){
+            showList.push({show:showName,episodes:[episodeNum],last:episodeNum});
           }
         }
-
-        //Find URL's from crunchyroll and include the word episode in the title
-        if(episodes[i].url.includes('crunchyroll.com') && episodes[i].title.includes('Episode')){
-          //Add an item to the HTML page unordered list
-          let histItem = document.createElement('li');
-          episodeNamenNum = episodes[i].title.replace("- Watch on Crunchyroll", '');
-          histItem.innerHTML = episodeNamenNum;
-          historyList.appendChild(histItem);
-          //Create an array of episode pages visited
-          if(!(episodeNamenNum in episodesVisited))
-          episodesVisited.push(episodes[i].title.match(episodeNamenNum));
-        }
       }
+
+          for(let k in showList){
+            if(tabShow === showList[k].show){
+              //Add an item to the HTML page unordered list
+              let histItem = document.createElement('li');
+              histItem.innerHTML = "Episode: " + showList[k].last;
+              historyList.appendChild(histItem);
+            }
+          }
+
+  })
+
+
+chrome.tabs.query({active: true,currentWindow: true,url: "https://www.crunchyroll.com/*"}, function(tabs) {
+  //if(tabs.includes('Episode')){
+    tabTitle = splitTitle(tabs[0]);
+    tabShow = tabTitle[0];
+  /*}else{
+    tabShow = tabs[0].title.replace(" - Watch on Crunchyroll",'');
+  }*/
 })
 
-console.log(episodesVisited);
-console.log(showsVisited);
+
+
+
+
+console.log(showList);
